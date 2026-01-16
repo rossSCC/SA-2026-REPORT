b9faa174ebd6dbf95291a186f360565fc9eab668
rossSCC <r.kearney20@swordscc.ie>
2026-01-16 10:52:24 +0000

    Idfk

---
+++ particles.js
@@ -0,0 +1,107 @@
+    // --- PARTICLES ---
+    // this is made on hopes and prayer that is works ive never used canvas before
+
+    // Get the canvas element and its context
+    const canvas = document.getElementById('hero-canvas');
+    const ctx = canvas.getContext('2d');
+
+    let particlesArray;
+
+    // Set canvas size to match the Hero section
+    function setCanvasSize() {
+        canvas.width = canvas.parentElement.offsetWidth;
+        canvas.height = canvas.parentElement.offsetHeight;
+    }
+    setCanvasSize();
+
+    // Create Particle Object
+    class Particle {
+        constructor() {
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+            // Random direction and speed
+            this.directionX = (Math.random() * 0.6) - 0.2;
+            this.directionY = (Math.random() * 0.4) - 0.2;
+            this.size = Math.random() * 2 + 1;
+            this.color = Math.random() > 0.5 ? '#ff6600' : '#ffffff'; 
+        }
+
+        // Method to draw individual particle
+        draw() {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+
+        // Check particle position, check mouse position, move the particle, draw the particle
+        update() {
+            // Check if particle is still within canvas
+            if (this.x > canvas.width || this.x < 0) {
+                this.directionX = -this.directionX;
+            }
+            if (this.y > canvas.height || this.y < 0) {
+                this.directionY = -this.directionY;
+            }
+
+            // Move particle
+            this.x += this.directionX;
+            this.y += this.directionY;
+
+            // Draw particle
+            this.draw();
+        }
+    }
+
+    // Create particle array
+    function init() {
+        particlesArray = [];
+        // Number of particles (based on screen size)
+        let numberOfParticles = (canvas.width * canvas.height) / 9000;
+        for (let i = 0; i < numberOfParticles; i++) {
+            particlesArray.push(new Particle());
+        }
+    }
+
+    // Animation Loop
+    function animate() {
+        requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+        }
+        connect();
+    }
+
+    // Check if particles are close enough to draw a line
+    function connect() {
+        let opacityValue = 1;
+        for (let a = 0; a < particlesArray.length; a++) {
+            for (let b = a; b < particlesArray.length; b++) {
+                let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x))
+                             + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
+
+                // If particles are closer than 150px, draw a line
+                if (distance < (canvas.width/7) * (canvas.height/7)) {
+                    opacityValue = 1 - (distance / 20000);
+                    ctx.strokeStyle = 'rgba(255, 102, 0,' + opacityValue + ')'; // Orange connection lines
+                    ctx.lineWidth = 1;
+                    ctx.beginPath();
+                    ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                    ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                    ctx.stroke();
+                }
+            }
+        }
+    }
+
+    // this fixes the issue w ith it not resizing
+    window.addEventListener('resize', function() {
+        setCanvasSize();
+        init();
+    });
+
+    // Call all the functions to start the animation
+    init();
+    animate();
